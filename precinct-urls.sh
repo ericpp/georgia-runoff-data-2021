a21940c48e71aaf7bd901c2455b0d34d033bd3fa
Automated Script <run@localhost>
2021-01-08 00:46:10 +0000

    precinct-urls.sh

--- precinct-urls.sh
+++ precinct-urls.sh
@@ -3,7 +3,7 @@
 urls=$(jq -r '.data.races[].precinct_metadata.timestamped_url' NYT/georgia.json  | grep -v null)
 
 for url in $urls; do
-	exists=$(grep NYT/precinct-urls.txt)
+	exists=$(grep "$url" NYT/precinct-urls.txt)
 
 	if [ -z "$exists" ]; then
 		echo "$url" >> NYT/precinct-urls.txt
